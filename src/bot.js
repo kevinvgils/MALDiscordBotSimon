969374d4236828d25886cceb49be64626a7d1ab8
Kevin van Gils <kvnvangils@gmail.com>
2022-04-25 22:25:28 +0200

    fixed error

--- src/bot.js
+++ src/bot.js
@@ -3,7 +3,6 @@ const { getAnime, getUser } = require('./calls.js')
 const { Client, Intents } = require('discord.js');
 const { DISCORD_API_KEY } = require('./../config.json');
 const exampleEmbed = require('./embedMessage.js');
-const generateImage = require('./userAttachment.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const PREFIX = '/';
 
